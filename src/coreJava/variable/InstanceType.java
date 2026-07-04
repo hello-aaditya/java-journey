7c76dd1d32b369c2203b42d80beb3ccab842f9f2
hello-aaditya <aadityakumar.prof@gmail.com>
2026-07-04 15:26:22 +0530

    implement AWS EC2 billing calculator using static, instance, and local variables

---
+++ src/coreJava/variable/InstanceType.java
@@ -0,0 +1,9 @@
+package coreJava.variable;
+
+public enum InstanceType {
+	T2_MICRO,
+	T3_SMALL,
+	M5_LARGE,
+	C5_XLARGE,
+	R5_2XLARGE
+}
---
+++ src/coreJava/variable/OperatingSystem.java
@@ -0,0 +1,9 @@
+package coreJava.variable;
+
+public enum OperatingSystem {
+	UBUNTU,
+	DEBIAN,
+	RHEL,
+	WINDOWS,
+	ARCH_LINUX
+}
