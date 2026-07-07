f88c52003151532b84ce1f168d1b733277329204
hello-aaditya <aadityakumar.prof@gmail.com>
2026-07-07 16:54:05 +0530

    Centralize company constants in CompanyInformation

---
+++ practice-java/src/coreJava/miniProjects/employeeIdCardGenerator/CompanyInformation.java
@@ -0,0 +1,14 @@
+package coreJava.miniProjects.employeeIdCardGenerator;
+
+public final class CompanyInformation {
+	
+	// STATIC VARIABLES
+	public static final String COMPANY_NAME = "TechNova Solutions Pvt. Ltd.";
+	public static final String COMPANY_PREFIX = "TN";
+	public static final String COMPANY_ADDRESS = "Bengaluru, Karnataka";
+	public static final String HR_EMAIL = "alpha@technova.com"; 
+	
+	private CompanyInformation() {
+		
+	}
+}
